0714656c79932501964fe29cb006acfd77399a5a
polygraphene <polygraphene@gmail.com>
2018-06-22 02:15:25 +0900

    Add missing files.

---
+++ app/src/Normal/java/com/polygraphene/alvr/ArThread.java
@@ -0,0 +1,42 @@
+package com.polygraphene.alvr;
+
+import android.opengl.EGLContext;
+
+class ArThread {
+    ArThread(VrThread vrThread, EGLContext mEGLContext) {
+    }
+
+    public void start(MainActivity activity) {
+    }
+
+    public void interrupt() {
+    }
+
+    public void join() {
+    }
+
+    public void initialize(MainActivity activity) {
+    }
+
+    public boolean onRequestPermissionsResult(MainActivity activity) {
+        return true;
+    }
+
+    public void setCameraTexture(int texture) {
+    }
+
+    public float[] getOrientation() {
+        return null;
+    }
+
+    public float[] getPosition() {
+        return null;
+    }
+
+    public String getErrorMessage() {
+        return null;
+    }
+
+    public static void requestPermissions(MainActivity activity) {
+    }
+}
